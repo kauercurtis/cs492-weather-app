d74133763c6a21525f1dc08f60c597dc1328adca
Curtis Kauer <kauercurtis@gmail.com>
2025-01-14 15:44:01 -0800

    assignment2-1

--- scripts/tests.dart
+++ scripts/tests.dart
@@ -13,6 +13,9 @@ void main() async {
   Map<String, dynamic> forecastJsonData = await getJsonFromUrl(forecastUrl);
   Map<String, dynamic> forecastHourlyJsonData = await getJsonFromUrl(forecastHourlyUrl);
 
+  processForecasts(forecastJsonData["properties"]["periods"]);
+  processForecasts(forecastHourlyJsonData["properties"]["periods"]);
+
   return;
 }
 
@@ -21,10 +24,14 @@ Future<Map<String, dynamic>> getJsonFromUrl(String url) async {
   return convert.jsonDecode(r.body);
 }
 
-void processForecasts(Map<String, dynamic> forecasts){
+void processForecasts(List<dynamic> forecasts){
   // TODO: pass the array of forcasts in from main
   // For loop through the forecasts and process each forecast with the
   // processForecast function below
+  // for(final forecast in forecasts)
+  for(var forecast in forecasts){
+    processForecast(forecast);
+  }
 }
 
 void processForecast(Map<String, dynamic> forecast){
@@ -32,5 +39,9 @@ void processForecast(Map<String, dynamic> forecast){
   // The proper values that will be useful. i.e. temperature, shortForecast, longForecast
   // for now, don't return anything, just assign values for each
   // i.e. String shortForcast = "";
-
+  String temperature = forecast["temperature"];
+  String temperatureUnit = forecast["temperatureUnit"];
+  String shortForcast = forecast["icon"]["shortForecast"];
+  String longForecast = forecast["icon"]["longForecast"];
+  
 }
