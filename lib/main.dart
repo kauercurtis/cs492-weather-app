dcc253c448d977c043323a778be34ad8a15ba474
Curtis Kauer <kauercurtis@gmail.com>
2025-01-30 15:39:45 -0800

    assignment4-2

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   List<forecast.Forecast> _forecastsHourly = [];
+  List<forecast.Forecast> _filteredForecastsHourly = []
   // create a new variable for _forecasts
+  List<forecast.Forecast> _forecasts = [];
   forecast.Forecast? _activeForecast;
   location.Location? _location;
 
@@ -81,6 +83,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   // TODO: create a new function getForecasts that returns forecast.getForecastFromPoints
+  Future<List<forecast.Forecast>> getForecasts(location.Location currentLocation) async {
+    return forecast.getForecastFromPoints(currentLocation.latitude, currentLocation.longitude);
+  }
 
   void setActiveHourlyForecast(int i){
     setState(() {
@@ -89,19 +94,25 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   // create a new function: setActiveHourlyForecast that updates _activeForecast with _forecasts[i]
-
+  void setActiveForecast(int i){
+    setState(() {
+      _activeForecast = _forecasts[i];
+    });
+  }
 
   void setLocation() async {
     if (_location == null){
       location.Location currentLocation = await location.getLocationFromGps();
 
       List<forecast.Forecast> currentHourlyForecasts = await getHourlyForecasts(currentLocation);
+      List<forecast.Forecast> currentForecasts = await getForecasts(currentLocation);
 
       setState(() {
         _location = currentLocation;
         _forecastsHourly = currentHourlyForecasts;
         _activeForecast = _forecastsHourly[10];
-        
+        _filteredForecastsHourly = getFilteredForecasts(0);
+        _forecasts = currentForecasts;
       });
     }
   }
@@ -132,12 +143,17 @@ class _MyHomePageState extends State<MyHomePage> {
               LocationWidget(location: _location),
               _activeForecast != null ? ForecastWidget(forecast: _activeForecast!) : Text(""),
               // TODO add a new ForecastSummariesWidget for the daily forecasts
-              _forecastsHourly.isNotEmpty ? ForecastSummariesWidget(forecasts: _forecastsHourly, setActiveForecast: setActiveHourlyForecast) : Text("")
+              _forecastsHourly.isNotEmpty ? ForecastSummariesWidget(forecasts: _forecastsHourly, setActiveForecast: setActiveHourlyForecast) : Text(""),
+              _forecasts.isNotEmpty ? ForecastSummariesWidget(forecasts: _forecasts, setActiveForecast: setActiveForecast) : Text("")
             ],
           ),
         ),
       ),
     );
+  }
+
+  List<forecast.Forecast> getFilteredForecasts(int i){
+    return _forecastsHourly.where((f)=>DateTime.parse(f.startTime!).day == DateTime.parse(_forecasts[i].startTime!).day).toList();
   }
 
 }
