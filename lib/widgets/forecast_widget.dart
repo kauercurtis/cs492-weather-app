a044cdc1996cce19892a0508beadef6fa41b3a55
Curtis Kauer <kauercurtis@gmail.com>
2025-01-30 14:06:21 -0800

    foo

--- lib/widgets/forecast_widget.dart
+++ lib/widgets/forecast_widget.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:weatherapp/scripts/forecast.dart' as forecast;
+import 'package:weatherapp/scripts/time.dart' as time;
+import 'package:weatherapp/scripts/math.dart' as math;
 
 class ForecastWidget extends StatelessWidget {
   const ForecastWidget({
     super.key,
-    required List<forecast.Forecast> forecasts,
-  }) : _forecasts = forecasts;
+    required forecast.Forecast forecast,
+  }) : _forecast = forecast;
 
-  final List<forecast.Forecast> _forecasts;
+  final forecast.Forecast _forecast;
 
   @override
   Widget build(BuildContext context) {
-    return Text(_forecasts.isNotEmpty ? _forecasts[0].shortForecast : "");
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          ForecastNameWidget(forecast: _forecast),
+          Text(_forecast.detailedForecast ?? _forecast.shortForecast),
+          Text("Wind: ${_forecast.windSpeed} ${_forecast.windDirection}"),
+          Text("Temp: ${_forecast.temperature}${_forecast.temperatureUnit}"),
+          Text("Dewpoint: ${math.roundToDecimalPlaces(_forecast.dewpoint, 2)}"),
+          Text("Humidity: ${_forecast.humidity}"),
+          Text("Chance of Rain: ${_forecast.precipitationProbability}"),
+      
+        ],
+      ),
+    );
+  }
+}
+
+class ForecastNameWidget extends StatelessWidget {
+  const ForecastNameWidget({
+    super.key,
+    required forecast.Forecast forecast,
+  }) : _forecast = forecast;
+
+  final forecast.Forecast _forecast;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _forecast.name ?? time.convertTimestampToDayAndHour(_forecast.startTime) ?? "",
+      style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold)
+    );
   }
 }
